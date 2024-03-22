94245a80700e2cfdc388a66b7cd94ae3f6a58ce5
Hirusha Deshan <89341648+HirushaDeshan@users.noreply.github.com>
2024-03-22 14:15:55 +0530

    cahanges done

--- js/globals.js
+++ js/globals.js
@@ -1,2 +1,2 @@
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbyFMvAwNBnB3oB5sAQxZ3ZvYin4sHFnye36fNQQ5eCdVxzWrDL0mvWW1MC3x6dRy6aE_A/exec";
+  "https://script.google.com/macros/s/AKfycbx_npNtb6umFFRUNIh5sSUDw5WYCpLogjX99_NBIsspqMRh3-_W-vnMQaC2BmW2RJQ/exec";
